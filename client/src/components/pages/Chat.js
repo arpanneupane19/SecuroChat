d806b5e895a778f905d745d4636e23b6ba00e084
Arpan Neupane <arpanneupane19@gmail.com>
2021-03-27 13:06:10 -0500

    chat frontend done!

--- client/src/components/pages/Chat.js
+++ client/src/components/pages/Chat.js
@@ -1,17 +1,14 @@
-import React, { useState, useParams } from 'react'
+import React, { useState } from 'react'
 import { Form, Input, Button } from 'antd';
-import { MessageTwoTone, LockOutlined, UserOutlined } from '@ant-design/icons';
+import { MessageTwoTone, LockOutlined, UserOutlined, MessageOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import './Chat.css'
-import { io } from 'socket.io-client'
+import { useParams } from 'react-router-dom';
 
-const socket = io('http://127.0.0.1:5000');
+function Chat(socket) {
+    let { code } = useParams();
+    console.log({ code });
 
-socket.on('connect', () => {
-    let connected = "You have connected!"
-})
-
-function Chat() {
     return (
         <div className='container'>
             <div className='header'>
@@ -19,7 +16,57 @@ function Chat() {
             </div>
 
             <div className='message-box'>
-                <p></p>
+                <div className='messages-header'>
+                    <p>Messages <MessageOutlined /></p>
+                </div>
+
+                <div className='messages'>
+                    <div className='message'>
+                        <div>2:14 pm</div>
+                        <div>Arpan: Hello world!</div>
+                    </div>
+                </div>
+
+
+                <div className='message-form'>
+                    <Form
+                        style={{ marginTop: '20px', display: 'flex' }}
+                        name="basic"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        size="large"
+                    >
+                        <Form.Item
+                            placeholder="Message"
+                            name="message"
+                            style={{ textAlign: 'left !important', marginRight: '8px' }}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your message!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={
+                                    <MessageTwoTone
+                                        className="site-form-item-icon"
+                                        style={{ marginRight: '8px' }}
+                                    />
+                                }
+                                placeholder="Message"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                        >
+                            <Button style={{ width: '100%' }} type="primary" htmlType="submit">
+                                Send
+                        </Button>
+                        </Form.Item>
+
+                    </Form>
+                </div>
             </div>
         </div>
     )
