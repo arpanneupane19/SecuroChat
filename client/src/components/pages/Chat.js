4764e08984905ebb330f532550cc842915967164
Arpan Neupane <arpanneupane19@gmail.com>
2021-04-01 00:41:51 -0500

    using useState for pop up messages

--- client/src/components/pages/Chat.js
+++ client/src/components/pages/Chat.js
@@ -1,42 +1,63 @@
-import React, { useState } from 'react'
-import { Form, Input, Button } from 'antd';
-import { MessageTwoTone, ArrowRightOutlined, SendOutlined, MessageOutlined, DoubleRightOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react'
+import { Form, Input, Button, message as alert } from 'antd';
+import { MessageTwoTone, ArrowRightOutlined, SendOutlined, MessageOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import './Chat.css'
 import { useParams } from 'react-router-dom';
-import { io } from 'socket.io-client';
 import styled from "styled-components";
 import moment from 'moment';
 
-const socket = io('http://127.0.0.1:5000')
 
-function Chat() {
+const Message = styled.div`
+background-color: rgb(0,140,255);
+padding: 8px;
+color: #fff;
+border-radius: 4px;
+`
+
+const Time = styled.div`
+`
+
+const MessageBody = styled.div`
+`
+
+function Chat({ socket }) {
     let { code } = useParams();
     console.log({ code });
     const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [codeExists, setCodeExists] = useState(false);
 
-    socket.on('connect', () => {
-        let username = localStorage.getItem('username')
-        console.log("User has successfully connected.")
-        socket.emit('connectUser', (username));
-    })
+    const fetchAPI = () => {
+        fetch(`http://127.0.0.1:5000/${code}`).then(
+            res => res.json()
+        ).then(
+            data => {
+                let response = data.code;
+                if (response === 'not found') {
+                    setCodeExists(false);
+                }
 
-    const Message = styled.div`
-        background-color: rgb(0,140,255);
-        padding: 8px;
-        color: #fff;
-        border-radius: 4px;
-    `
+                if (response !== 'not found') {
+                    setCodeExists(true);
+                }
 
-    const Time = styled.div`
-    `
+            }
+        )
+    }
 
-    const MessageBody = styled.div`
-    `
+    useEffect(() => {
+        socket.on('connect', () => {
+            let username = localStorage.getItem('username');
+            socket.emit('connectUser', (username))
+            alert.success('You have connected!')
+        })
 
-    socket.on('message', (message) => {
-        console.log(`${message.message} was sent by ${message.sender} at ${message.time}.`)
-    })
+        socket.on('sysMessage', (msg) => {
+            alert.info(`${msg}`)
+        })
+    }, [])
+
 
     return (
         <div className='container'>
@@ -52,8 +73,8 @@ function Chat() {
 
                 <div className='messages'>
                     <Message>
-                        <Time>2:14 pm</Time>
-                        <MessageBody>Arpan: Hello world!</MessageBody>
+                        <Time></Time>
+                        <MessageBody></MessageBody>
                     </Message>
                 </div>
 
@@ -93,7 +114,7 @@ function Chat() {
                         <Form.Item
                         >
 
-                            <Button title='Send Message' style={{ width: '100%', borderRadius: '4px' }} type="primary" htmlType="submit" onClick={() => socket.emit('message', { message: message, sender: localStorage.getItem('username'), time: moment().format('h:mm a') })}>
+                            <Button title='Send Message' style={{ width: '100%', borderRadius: '4px' }} type="primary" htmlType="submit" onClick={() => console.log('hi')}>
                                 Send <SendOutlined />
                             </Button>
                         </Form.Item>
@@ -104,5 +125,7 @@ function Chat() {
         </div >
     )
 }
+
+
 
 export default Chat
