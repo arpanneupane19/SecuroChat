7544a61c1c999e0c41814b432083dbdef03c84f9
Arpan Neupane <arpanneupane19@gmail.com>
2021-04-03 03:20:29 -0500

    added additional info modal

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Form.css'
-import { MessageTwoTone, LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Form, Input, Button, message } from 'antd';
+import { MessageTwoTone, LockOutlined, UserOutlined, QuestionOutlined } from '@ant-design/icons';
+import { Form, Input, Button, message, Modal } from 'antd';
 import 'antd/dist/antd.css';
 import { Link } from 'react-router-dom';
 
@@ -9,6 +9,15 @@ function Home({ socket }) {
     const [name, setName] = useState('');
     const [code, setCode] = useState('');
     const [codeExists, setCodeExists] = useState(false);
+    const [visible, setVisible] = useState(false);
+
+    const showModal = () => {
+        setVisible(true);
+    }
+
+    const closeModal = () => {
+        setVisible(false);
+    }
 
     const fetchAPI = () => {
         fetch(`http://127.0.0.1:5000/${code}`).then(
@@ -25,7 +34,6 @@ function Home({ socket }) {
                     setCodeExists(true);
                     message.error('Room code already exists.')
                 }
-
             }
         )
     }
@@ -38,7 +46,28 @@ function Home({ socket }) {
                 <h1><MessageTwoTone /> SecuroChat <MessageTwoTone /></h1>
             </div>
             <div className='form'>
-                <p>Create Room</p>
+                <div className='form-header'>
+                    <p>Create Room</p>
+                    <Button onClick={showModal} shape="circle" icon={<QuestionOutlined style={{ color: 'darkslategray' }} />} />
+                    <Modal title="Additional Information" visible={visible} onOk={closeModal} onCancel={closeModal}
+                        footer={[
+                            <Button type="primary" key="back" onClick={closeModal}>
+                                OK
+                            </Button>,
+                        ]}
+                    >
+                        <p>
+                            To create a room, enter in a username, this username is what you'll be recognized by.
+                            Then enter in a room code, this can be numbers, letters, or symbols. Once you create a room,
+                            you'll be in the chat room. You can invite your friends over to join and you can chat freely without
+                            having to worry about privacy!
+                        </p>
+                        <div>
+                            <p>"We don't store any of your data."</p>
+                            <p>- <a href="http://arpanneupane.com" target="__blank">Arpan Neupane</a> (Creator of SecuroChat)</p>
+                        </div>
+                    </Modal>
+                </div>
                 <Form
                     name="basic"
                     initialValues={{
