e3decbcf5354af1daa6bde46c2410e3e0e97c5e6
Arpan Neupane <arpanneupane19@gmail.com>
2021-04-03 03:20:38 -0500

    additional info modal works

--- client/src/components/pages/Join.js
+++ client/src/components/pages/Join.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Form.css'
-import { MessageTwoTone, LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Form, Input, Button, message } from 'antd';
+import { MessageTwoTone, LockOutlined, UserOutlined, QuestionOutlined } from '@ant-design/icons';
+import { Form, Input, Button, message, Modal } from 'antd';
 import 'antd/dist/antd.css';
 import { Link } from 'react-router-dom';
 
@@ -10,6 +10,15 @@ function Join({ socket }) {
     const [name, setName] = useState('');
     const [code, setCode] = useState('');
     const [codeExists, setCodeExists] = useState(false);
+    const [visible, setVisible] = useState(false);
+
+    const showModal = () => {
+        setVisible(true);
+    }
+
+    const closeModal = () => {
+        setVisible(false);
+    }
 
     const fetchAPI = () => {
         fetch(`http://127.0.0.1:5000/${code}`).then(
@@ -40,7 +49,29 @@ function Join({ socket }) {
                 <h1><MessageTwoTone /> SecuroChat <MessageTwoTone /></h1>
             </div>
             <div className='form'>
-                <p>Join Room</p>
+                <div className='form-header'>
+                    <p>Join Room</p>
+                    <Button onClick={showModal} shape="circle" icon={<QuestionOutlined style={{ color: 'darkslategray' }} />} />
+                    <Modal title="Additional Information" visible={visible} onOk={closeModal} onCancel={closeModal}
+                        footer={[
+                            <Button type="primary" key="back" onClick={closeModal}>
+                                OK
+                            </Button>,
+                        ]}
+                    >
+                        <p>
+                            To join a room, enter in a username, this is the name you'll be recognized by other people in the room.
+                            Then enter in a valid room, it has to exist in order for you to join. Once you're in the room, you can chat
+                            freely without worrying about privacy!
+                        </p>
+
+                        <div>
+                            <p>"We don't store any of your data."</p>
+                            <p>- <a href="http://arpanneupane.com" target="__blank">Arpan Neupane</a> (Creator of SecuroChat)</p>
+                        </div>
+
+                    </Modal>
+                </div>
                 <Form
                     name="basic"
                     initialValues={{
